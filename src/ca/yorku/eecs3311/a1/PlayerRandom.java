0e7b3a39176f579ea771737518fd54fc0a7ec6f6
Werner Bagu-Paiz <totaldarkness@totaldarkness.net>
2024-09-21 01:34:09 -0400

    add basic PlayerRandom logic

--- src/ca/yorku/eecs3311/a1/PlayerRandom.java
+++ src/ca/yorku/eecs3311/a1/PlayerRandom.java
@@ -12,11 +12,35 @@ import java.util.Random;
  *
  */
 public class PlayerRandom {
-	
+	private Othello othello;
+	private char player;
 	private Random rand = new Random();
 
+	public PlayerRandom(Othello othello, char player) {
+		this.othello = othello;
+		this.player = player;
+	}
+
 	public Move getMove() {
-		return null;
+		if (othello.getWhosTurn() != player) {
+			return null;
+		}
+		char hasMove = othello.getBoard().hasMove();
+		// TODO acceptable to send null?
+		if (hasMove == OthelloBoard.EMPTY || hasMove == OthelloBoard.otherPlayer(player)) {
+			return null;
+		}
+		ArrayList<Move> moves = new ArrayList<>();
+		for (int row = 0; row < othello.getBoard().getDimension(); row++) {
+			for (int col = 0; col < othello.getBoard().getDimension(); col++) {
+				hasMove = othello.getBoard().hasMove(row, col);
+				if (hasMove == OthelloBoard.BOTH || hasMove == player) {
+					moves.add(new Move(row, col));
+				}
+			}
+		}
+		if (moves.isEmpty()) return null;
+		return moves.get(rand.nextInt(moves.size()));
 	}
 }
 
