2b60e95b88b5b8ecc2e79d92c6f9a633ce16532d
TotalDarkness <totaldarkness@totaldarkness.net>
2024-09-22 17:19:52 -0400

    add javadoc

--- src/ca/yorku/eecs3311/a1/PlayerGreedy.java
+++ src/ca/yorku/eecs3311/a1/PlayerGreedy.java
@@ -22,18 +22,21 @@ package ca.yorku.eecs3311.a1;
  */
 public class PlayerGreedy extends Player {
 
+	/**
+	 * Gets a move by considering all possible moves that the player.
+	 * Picks the moves that will increase the number tokens the most.
+	 * If two moves have the same max, the move with the smallest row is chosen.
+	 * If two moves also have the same row, the move with the smallest col is chosen.
+	 *
+	 * @return the move that will increase the number of tokens the most.
+	 */
 	public Move getMove() {
-		// TODO do we need to check if its my turn?
-		if (this.othello.getWhosTurn() != player) {
-			return null;
-		}
+		if (this.othello.getWhosTurn() != player) return null;
 		char hasMove = othello.getBoard().hasMove();
-		// TODO acceptable to send null?
 		if (hasMove == OthelloBoard.EMPTY || hasMove == OthelloBoard.otherPlayer(player)) {
 			return null;
 		}
-		int maxPieces = 0;
-		int count;
+		int count, maxPieces = 0;
 		Move move = null;
 		for (int row = 0; row < othello.getBoard().getDimension(); row++) {
 			for (int col = 0; col < othello.getBoard().getDimension(); col++) {
@@ -46,7 +49,6 @@ public class PlayerGreedy extends Player {
 							else if (row == move.getRow()) {
 								if (col > move.getCol()) continue;
 							}
-							// TODO code is so ugly need to refactor later
 						}
 						maxPieces = count;
 						move = new Move(row, col);
@@ -57,10 +59,18 @@ public class PlayerGreedy extends Player {
 		return maxPieces == 0 ? null : move;
 	}
 
+	/**
+	 * Gets the count of tokens that will be added to the player
+	 * if the player makes a specified move in row, col and in direction
+	 * drow, dcol.
+	 * @param row starting row, in {0,...,dim-1} (typically {0,...,7})
+	 * @param col starting col, in {0,...,dim-1} (typically {0,...,7})
+	 * @param drow the row direction, in {-1,0,1}
+	 * @param dcol the col direction, in {-1,0,1}
+	 * @return the number of tokens that will be added as a result of this move.
+	 */
 	private int getMoveCounts(int row, int col, int drow, int dcol) {
-		if (drow < -1 || drow > 1) return 0;
-		if (dcol < -1 || dcol > 1) return 0;
-		if (drow == 0 && dcol == 0) return 0;
+		if (!othello.getBoard().validDirection(drow, dcol)) return 0;
 		int count = 0;
 		char piece;
 		do {
@@ -77,6 +87,13 @@ public class PlayerGreedy extends Player {
 		return 0;
 	}
 
+	/**
+	 * Gets the max number of tokens that will be added from this move
+	 * at the specified row and col.
+	 * @param row starting row, in {0,...,dim-1} (typically {0,...,7})
+	 * @param col starting col, in {0,...,dim-1} (typically {0,...,7})
+	 * @return the max number of tokens that will be added as a result of this move.
+	 */
 	private int getMoveCounts(int row, int col) {
 		int count = 0;
 		for (int drow = -1; drow <= 1; drow++) {
--- src/ca/yorku/eecs3311/a1/RandomVsRandom.java
+++ src/ca/yorku/eecs3311/a1/RandomVsRandom.java
@@ -2,11 +2,28 @@ package ca.yorku.eecs3311.a1;
 
 import java.util.Random;
 
+/**
+ * Attempts to show that the null hypothesis is correct.
+ * Uses bootstrap resampling on a few simulated games to determine the probability
+ * that a specified player wins.
+ * Bootstrap resampling uses a sample, creates new samples using random data from the sample,
+ * in order to create new data from the sample. It repeats this many times to get a new result.
+ * Using the class shows that when playing random players against each other, the probability
+ * does not change by much for each player, showing that P1 has a disadvantage compared to P2.
+ */
 public class RandomVsRandom {
+
+    /**
+     * Gets the probability that a player wins a game,
+     * simulating the desired number of games.
+     * @param numGames the number of games to simulate.
+     * @param player the player to record wins.
+     * @return the probability the player wins from these simulated games
+     */
     private static float getProbabilityPlayerWins(int numGames, char player) {
         int playerWins = 0;
         for (int i = 0; i < numGames; i++) {
-            OthelloControllerRandomVSRandom oc = new OthelloControllerRandomVSRandom();
+            OthelloController oc = new OthelloControllerRandomVSRandom();
             oc.play();
             char winner = oc.othello.getWinner();
             if (player == winner) {
@@ -16,6 +33,13 @@ public class RandomVsRandom {
         return (float) playerWins / numGames;
     }
 
+    /**
+     * Gets the sample data using the number of samples requested,
+     * for the requested player
+     * @param numSamples the number of samples to generate.
+     * @param player the player to record wins.
+     * @return the samples of the simulated games for player winning.
+     */
     private static float[] getSample(int numSamples, char player) {
         float[] sample = new float[numSamples];
         for (int i = 0; i < numSamples; i++) {
@@ -24,17 +48,20 @@ public class RandomVsRandom {
         return sample;
     }
 
+    /**
+     * Simulates by default a set of 50 samples of 50 games, calculating the
+     * probability that a player wins. Then uses bootstrap resampling to quickly
+     * generate more data from the original to create a better estimate of the
+     * probability that the player wins. When this method is run multiple times
+     * it is clear that the probability that the player wins does not change by much.
+     * @param args
+     */
     public static void main(String[] args) {
         char player = OthelloBoard.P2;
         int numSamples = 50, sampleSize = 100000;
         Random rand = new Random();
         float[] sample = getSample(numSamples, player);
         float sumOfMeans = 0;
-        /*
-        bootstrap method gets a sample
-        then draws random samples, calculate average
-        do this for a number of times
-         */
         for (int i = 0; i < sampleSize; i++) {
             float sum = 0;
             for (int j = 0; j < numSamples; j++) {
