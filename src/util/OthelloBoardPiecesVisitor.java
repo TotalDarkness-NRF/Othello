70129963407a9d65a644974d249c97362e5c25d9
TotalDarkness <totaldarkness@totaldarkness.net>
2024-12-03 17:44:01 -0500

    add OthelloPieces visitor to find pieces on the board

---
+++ src/util/OthelloBoardPiecesVisitor.java
@@ -0,0 +1,25 @@
+package util;
+
+import othello.model.Move;
+import othello.model.OthelloBoard;
+
+import java.util.HashMap;
+
+public class OthelloBoardPiecesVisitor implements Visitor {
+    private final HashMap<Move, Character> pieces = new HashMap<>();
+
+    @Override
+    public void visit(OthelloBoard othelloBoard) {
+        for (int row = 0; row < othelloBoard.getDimension(); row++) {
+            for (int col = 0; col < othelloBoard.getDimension(); col++) {
+                char piece = othelloBoard.get(row, col);
+                if (piece == OthelloBoard.EMPTY) continue;
+                pieces.put(new Move(row, col), piece);
+            }
+        }
+    }
+
+    public HashMap<Move, Character> getPieces() {
+        return pieces;
+    }
+}
