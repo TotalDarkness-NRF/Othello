28b1acf41fed7a0ac4ef863a947807bc2d5a6bef
TotalDarkness <totaldarkness@totaldarkness.net>
2024-12-03 14:14:00 -0500

    fix open close title

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public class FileUtil {
     public static Optional<File> chooseFile(Stage stage, boolean save) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save Othello Board");
+        fileChooser.setTitle((save ? "Save" : "Open") + " Othello Board");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Othello Files", "*.othello"));
         File file = save ? fileChooser.showSaveDialog(stage) : fileChooser.showOpenDialog(stage);
         return Optional.ofNullable(file);
